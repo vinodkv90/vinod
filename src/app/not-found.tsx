ddca3f9fa18ada691b29f66e3989e0961a489f09
Vinod <vinodkv1990@gmail.com>
2025-10-10 07:33:04 +0530

    changes 6

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,4 @@
 import Button from '@/components/Button'
-import Link from 'next/link'
  
 export default function NotFound() {
   return (
@@ -7,8 +6,7 @@ export default function NotFound() {
         <div className="container mx-auto">
             <h1 className='font-dela-gothic text-6xl m-0 text-foreground'>404</h1>
             <h2 className='font-dela-gothic text-xl mt-4 mb-0 text-foreground'>Page Not Found</h2>
-            {/* <Button as='a' href="/" className='w-max !capitalize mx-auto !tracking-wider mt-6' size='sm'>Return Home</Button> */}
-            <Link href="/" className='btn-primary btn-sm cursor-pointer font-montserrat rounded-sm px-4 py-0 flex items-center justify-center text-xs text-white font-medium uppercase leading-[1] w-max !capitalize mx-auto !tracking-wider mt-6'>Return Home</Link>
+            <Button as='a' href="/" className='w-max !capitalize mx-auto !tracking-wider mt-6' size='sm'>Return Home</Button>
         </div>
     </div>
   )
